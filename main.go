60eea4acaa203fe5e1f9d0f092b1b30edf9c3d4e
Haipeng <njucs_wuhaipeng@163.com>
2015-07-02 16:50:32 +0800

    support influxdb sample running on daocloud

--- main.go
+++ main.go
@@ -2,38 +2,144 @@ package main
 
 import (
     "log"
+    "fmt"
+    "time"
+    "os"
+    "net/http"
+    "net/url"
 
     "github.com/influxdb/influxdb/client"
 )
 
 var ic *client.Client
 
-func connectStorage() {
-    host, err := url.Parse(fmt.Sprintf("http://%s:%d", "localhost", 8086))
+var host string
+var port string
+var db string
+var user string
+var password string
+
+func connect() {
+    u, err := url.Parse(fmt.Sprintf("http://%s:%s", host, port))
     if err != nil {
         log.Fatal(err)
     }
 
-    con, err := client.NewClient(client.Config{URL: *host})
+    ic, err = client.NewClient(client.Config{URL: *u})
     if err != nil {
         log.Fatal(err)
     }
 
-    dur, ver, err := con.Ping()
-    if err != nil {
+    if _, _, err := ic.Ping(); err != nil {
         log.Fatal(err)
     }
 
-    log.Printf("Happy as a hippo! %v, %s", dur, ver)
-    ic = con
+    ic.SetAuth(user, password)
+}
+
+func create() {
+    // Workaround, since daocloud influxdb haven't privision an instance
+    // create the db instance here
+    q := client.Query{
+        Command:  fmt.Sprintf("create database %s", db),
+        Database: db,
+    }
+
+    // ignore the error of existing database
+    ic.Query(q)
+}
+
+
+func insert() {
+    var (
+        shapes     = []string{"circle", "rectangle", "square", "triangle"}
+        colors     = []string{"red",    "blue",      "green",  "yellow"  }
+        sampleSize = 4
+        pts        = make([]client.Point, sampleSize)
+    )
+
+    for i := 0; i < sampleSize; i++ {
+        pts[i] = client.Point{
+            Measurement: "shapes",
+            Tags: map[string]string{
+                "color": colors[i],
+                "shape": shapes[i],
+            },
+            Fields: map[string]interface{}{
+                "value": i,
+            },
+            Time: time.Now(),
+        }
+    }
+
+    bps := client.BatchPoints{
+        Points:          pts,
+        Database:        db,
+        RetentionPolicy: "default",
+    }
+
+    _, err := ic.Write(bps)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func query() map[string]string {
+    q := client.Query{
+        Command:  "select * from shapes where value = 0",
+        Database: db,
+    }
+
+    response, err := ic.Query(q)
+    if err != nil {
+        log.Println("Error, ", err)
+        return nil
+    }
+
+    if response.Error() != nil {
+        log.Println("Response error, ", response.Error())
+        return nil
+    }
+
+    result := response.Results[0]
+    if result.Err != nil {
+        log.Println("Result error, ", result.Err)
+        return nil
+    }
+
+    serie := result.Series[0]
+    if serie.Err != nil {
+        log.Println("Serie error, ", serie.Err)
+        return nil
+    }
+
+    return serie.Tags
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-    res.Write(fmt.Sprintf("Hello World, %d!", 1234))
+    m := query()
+    res.Write([]byte(fmt.Sprintf("The first shape is %s %s!", m["color"], m["shape"])))
 }
 
 func main() {
-    connectStorage()
+    host = os.Getenv("INFLUXDB_PORT_8086_TCP_ADDR")
+    port = os.Getenv("INFLUXDB_PORT_8086_TCP_PORT")
+    db = os.Getenv("INFLUXDB_INSTANCE")
+    user = os.Getenv("INFLUXDB_USERNAME")
+    password = os.Getenv("INFLUXDB_PASSWORD")
+
+    // workaround, daocloud influxdb have not privision db instance
+    if len(db) == 0 {
+        db = "mydb"
+    }
+
+    connect()
+    log.Println("Successfully connect to influxdb ...")
+
+    // prepare data
+    create()
+    insert()
+
     http.HandleFunc("/", hello)
 
     log.Println("Start listening...")
@@ -41,5 +147,3 @@ func main() {
         log.Fatal(err)
     }
 }
-
-
